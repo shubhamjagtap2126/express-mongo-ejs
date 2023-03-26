7f323b5f00d1a122150ec373960c91081636d659
Shubham Jagtap <shubhamjagtap2126@gmail.com>
2023-03-26 10:33:21 +0530

    V1 Basic Landing

--- app.js
+++ app.js
@@ -9,49 +9,33 @@ const express = require("express");
 const path = require("path");
 const ejsMate = require("ejs-mate");
 const mongoose = require("mongoose");
-const session = require("express-session");
-const flash = require("connect-flash");
-const createError = require("http-errors");
-const cookieParser = require("cookie-parser");
-const logger = require("morgan");
-const helmet = require("helmet");
-const mongoSanitize = require("express-mongo-sanitize");
-const ExpressError = require("./utils/ExpressError");
+
 
 // Routes
-const indexRouter = require("./routes/index");
-const usersRouter = require("./routes/users");
+const homeRouter = require("./routes/home");
+
+const dbUrl = process.env.DB_URL;
+
+mongoose
+  .connect(dbUrl, { useNewUrlParser: true })
+  .then(console.log("⚡️[Database]: connected"))
+  .catch((err) => console.log(err));
+
+mongoose.set("strictQuery", false);
 
 const app = express();
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
+app.engine("ejs", ejsMate);
+app.set("view engine", "ejs");
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(express.urlencoded({ extended: true }));
 
-// Routes Handling
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
+app.use("/", homeRouter);
 
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("layouts/error");
-});
 
 const port = 4000;
 app.listen(port, () => {
